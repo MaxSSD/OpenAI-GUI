020e47200031f5c1748291756e9fea754acdb995
Max <86234226+MaxSSD@users.noreply.github.com>
2023-01-09 08:03:59 +0100

    Update openaigui.py

--- openaigui.py
+++ openaigui.py
@@ -1,6 +1,7 @@
 import logging
 import PySimpleGUI as sg
 import openai
+import os
 from api_key import key
 from imgnsound import noacc, speak
 
@@ -10,69 +11,64 @@ from imgnsound import noacc, speak
 # Defines the modules() and openAi() functions which are used to select the engine and generate a response.
 def modules(engines):
     if engines == "text-davinci-003":
-        m = "text-davinci-003"
+        model = "text-davinci-003"
     elif engines == "text-davinci-002":
-        m = "text-davinci-002"
+        model = "text-davinci-002"
     elif engines == "text-curie-001":
-        m = "text-curie-001"
+        model = "text-curie-001"
     elif engines == "text-babbage-001":
-        m = "text-babbage-001"
+        model = "text-babbage-001"
     elif engines == "text-ada-001":
-        m = "text-ada-001"
-    return m
+        model = "text-ada-001"
+    return model
 
 def openAi(engines, prompt_in):
-    # Use the selected module in the engine parameter to choose AI engine.
     completion = openai.Completion.create(engine=modules(engines), prompt=prompt_in, temperature=0, max_tokens=377,
                                          top_p=1.0, frequency_penalty=0.0, presence_penalty=0.0)
     result = completion.choices[0].text
-    length = ((result).join(result)).count(result) + 1
-    # Creating an object response
     logger = logging.getLogger()
     logging.basicConfig(filename='answers.txt', level=logging.INFO)
-    # Condition to respond with audio or in file.
-    if length < 150:
+    if len(result) < 150:
         sg.Popup('Responding...', keep_on_top=True)
         speak(result)
         logger.info(result)
     else:
-        sg.Popup('Responding to answer.txt', keep_on_top=True)
+        sg.Popup('Responding to answers.txt', keep_on_top=True)
         speak(noacc)
-        logger.info(result)
+        with open('answers.txt', 'a+') as f:
+            f.write(result)
 
 def make_window(theme):
     sg.theme(theme)
     # GUI layout.
-    layout_prompt = [[sg.Radio('Choose model', 'RADIO1', key='modules'),
-                      sg.Combo(['text-davinci-003', 'text-davinci-002', 'text-curie-001', 'text-babbage-001',
-                                'text-ada-001'], key='engines')],
-                     [sg.Text('Enter your question or statement below:', font=("Arial", 9, 'bold'))],
-                     [sg.Multiline(key="prompt", size=(77, 20))],
-                     # [sg.Input(key='prompt', size=(77, 1), do_not_clear=True)],
-                     [sg.Button('Answer'), sg.Button('Clear'), sg.Button('Quit')]]
-
-    layout_theme = [[sg.Text('Choose theme:')],
-                    [sg.Listbox(values=sg.theme_list(), size=(20, 12), key='-THEME LISTBOX-', enable_events=True)],
-                    [sg.Button("Set Theme")]]
-
-    layout_about = [[sg.Text('text-davinci-003 - Upgraded davinci-002. GPT3 chatbot model.')],
-                    [sg.Text('text-davinci-002 - Complex intent, cause and effect, summarization for audience')],
-                    [sg.Text('text-curie-001 - Language translation, complex classification, text sentiment, summarization')],
-                    [sg.Text('text-babbage-001 - Moderate classification, semantic search classification')],
-                    [sg.Text('text-ada-001 - Parsing text, simple classification, address correction, keywords')]]
-    # Layout sizing.
-    layout = [[sg.Text('OpenAIGUI', size=(63, 1), justification='center', font=("Helvetica", 13),
-                       relief=sg.RELIEF_RIDGE, k='-TEXT HEADING-', enable_events=True)]]
-
-    layout += [[sg.TabGroup([[sg.Tab('OpenAi', layout_prompt),
-                              sg.Tab('Theme', layout_theme),
-                              sg.Tab('About', layout_about)
-                              ]], key='-TAB GROUP-', expand_x=True, expand_y=True)]]
+    layout = [
+        [sg.Text("OpenAIGUI", size=(63, 1), justification="center", font=("Helvetica", 13), relief=sg.RELIEF_RIDGE, key="-TEXT HEADING-", enable_events=True)],
+        [sg.TabGroup([
+            [sg.Tab("OpenAi", [
+                [sg.Radio("Choose model", "RADIO1", key="modules"), sg.Combo(["text-davinci-003", "text-davinci-002", "text-curie-001", "text-babbage-001", "text-ada-001"], key="engines")],
+                [sg.Text("Enter your question or statement below:", font=("Arial", 9, 'bold'))],
+                [sg.Multiline(key="prompt", size=(77, 20))],
+                [sg.Button("Answer"), sg.Button('Open file'), sg.Button("Clear"), sg.Button("Quit")]
+            ]),
+            sg.Tab("Theme", [
+                [sg.Text("Choose theme:")],
+                [sg.Listbox(values=sg.theme_list(), size=(20, 12), key="-THEME LISTBOX-", enable_events=True)],
+                [sg.Button("Set Theme")]
+            ]),
+            sg.Tab("About", [
+                [sg.Text("text-davinci-003 - Upgraded davinci-002. GPT3 chatbot model.")],
+                [sg.Text("text-davinci-002 - Complex intent, cause and effect, summarization for audience")],
+                [sg.Text("text-curie-001 - Language translation, complex classification, text sentiment, summarization")],
+                [sg.Text("text-babbage-001 - Moderate classification, semantic search classification")],
+                [sg.Text("text-ada-001 - Parsing text, simple classification, address correction, keywords")]
+            ])
+        ]], key="-TAB GROUP-", expand_x=True, expand_y=True)
+    ]]
     layout[-1].append(sg.Sizegrip())
-
     # Gui window and layout sizing.
-    window = sg.Window('OpenAI GUI', layout, resizable=True, return_keyboard_events=True, use_custom_titlebar=True, finalize=True, keep_on_top=True)
-    # window.bind(bind_string="<Enter>", key="Answer", propagate=True)
+    # icon='C:/OpenAI-GUI/icon.ico'
+    window = sg.Window('OpenAI GUI', layout, resizable=True, return_keyboard_events=True, finalize=True)
+    # window.bind('<Configure>', "Configure")
     return window
 
 # GUI window that runs the main() function to interact with the user.
@@ -80,6 +76,7 @@ def main():
     window = make_window(sg.theme())
 
     # Set keyboard shortcuts for the buttons [ADD KEY BINDS]
+    # window.bind(bind_string="<Enter>", key="Answer", propagate=True)
     # window['Answer'].bind('<Return>', 'Answer')
     # window['Clear'].bind('<Delete>', 'Clear')
     # window['Quit'].bind('<Escape>', close_me)
@@ -92,6 +89,8 @@ def main():
         if event == 'Answer':
             prompt_in = values['prompt']
             openAi(engines, prompt_in)
+        elif event == 'Open file':
+            os.startfile('answers.txt', 'open')
         elif event == 'Clear':
             window['prompt'].update('')
         elif event == "Set Theme":
